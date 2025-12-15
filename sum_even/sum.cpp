0ba6a7f06621faa98d04510d5d03a6f82c9fe52d
hamza <hamzashzd725@gmail.com>
2025-12-15 22:10:57 +0500

    Adding sum_even

---
+++ sum_even/sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num, sum = 0;
+  cout<<"Enter upper limit for sum of even numbers: ";
+  cin>>num;
+
+  for (int i = 0; i <=num; i++)
+  {
+    if (i % 2 != 0)
+    {
+      continue;
+    }
+    sum = sum + i;
+  }
+
+  cout<<"Sum is "<<sum<<endl;
+
+  return 0;
+}
