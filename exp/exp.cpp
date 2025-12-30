db23c08cd293d26601817b27e19c14af9b66c01b
Hamza Shahzad <hamzashzd725@gmail.com>
2025-12-30 17:59:21 +0500

    Testing Wsl

---
+++ exp/exp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers: ";
+    int n1, n2;
+    cin>>n1>>n2;
+
+    cout<<"n1 sq: "<<n1*n1<<"\nn2 sq: "<<n2*n2<<endl;
+    return 0;
+}
