a189c3e17dbc89314ae608cc6574a43b44312f19
Hamza Shahzad <hamzashzd725@gmail.com>
2026-01-05 22:58:22 +0500

    Adding duck command

--- duck/README.md
+++ duck/README.md
@@ -3,8 +3,10 @@ Checks if a number is a duck number
 
 ##Sample Output
 
-Enter a number to check if it is a duck number or not: 00004567020
+Enter a number to check if it is a duck number or not: 4400334400123
 
 Number is Duck Number.
 
-Number of zeros is: 2
+Number of zeros is: 4
+
+Places from right: 3
--- duck/duck.cpp
+++ duck/duck.cpp
@@ -3,24 +3,27 @@ using namespace std;
 
 int main()
 {
-    int number;
+    long long number;
 
     cout<<"Enter a number to check if it is a duck number or not: ";
     cin>>number;
-    int temp = number;
-    int count = 0;
+    long long temp = number;
+    int count = 0, places = 0, total = 0; 
     while(temp > 1)
     {
+        if (count < 1)
+            places++;
         if (temp % 10 == 0)
         {
             count++;
         }
-
         temp = temp/10;
     }
-    if (count > 0){
+    if (count > 0)
+    {
         cout<<"Number is Duck Number.\n";
         cout<<"Number of zeros is: "<<count;
+        cout<<"\nPlaces from right: "<<places - 1;
     }
     else
         cout<<"Number is not a Duck Number";
